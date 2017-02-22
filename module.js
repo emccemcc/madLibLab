656901e0328206051fbc5529a45be6d23bba6439
Eric McCormick <Eric McCormick>
2017-02-22 17:16:10 -0500

    adding routing to module

--- module.js
+++ module.js
@@ -1,1 +1,15 @@
 var app = angular.module('myModule', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/input', {
+        controller: 'Ctrl1',
+        templateUrl: 'input.html'
+    })
+    .when('/result', {
+      controller: 'Ctrl2',
+      templateUrl: 'result.html'
+    });
+
+
+});
