99a5c537221f418ca03a23b6c021a9b8811154ad
RedEis91 <imitationkrabs91@gmail.com>
2017-02-23 00:10:59 -0500

    added depth, fixed variable / object / function names to suit purposes

--- controller.js
+++ controller.js
@@ -2,17 +2,28 @@ var app = angular.module('myModule');
 
 app.controller('Ctrl1', function($scope, $location, ourFactory){
 
-  $scope.fullName = function(firstName, middleName, lastName){
-      var displayFullName = {
-        first: firstName,
-        second: middleName,
-        third: lastName
+  $scope.fullSong = function(place, activity, activityPlural, pronoun, nickname, rhyme1, people, verb2, need, money, disease, bring, knees, bleed){
+      var createSong = {
+        first: place,
+        second: activity,
+        third: activityPlural,
+        fourth: pronoun,
+        fifth: nickname,
+        sixth: rhyme1,
+        seventh: people,
+        eighth: verb2,
+        ninth: need,
+        tenth: money,
+        eleventh: disease,
+        twelth: bring,
+        thirteenth: knees,
+        fourteenth: bleed
           
       };
-console.log(displayFullName);
+console.log(createSong);
 
 
-      ourFactory.ourFunction(displayFullName);
+      ourFactory.ourFunction(createSong);
 console.log(ourFactory.returnWord());
 $location.path('/result');
 
--- factory.js
+++ factory.js
@@ -1,21 +1,37 @@
 var app = angular.module('myModule');
 
-app.factory('ourFactory', function() {
+app.factory('ourFactory', function () {
 
 
- var finalData = '';
-//returning initial object
-return {
-  ourFunction: function(displayFullName){
-  finalData = "Welcome to the "  + displayFullName.first + '. We\'ve got '+ displayFullName.second + ' and ' + displayFullName.third + '!';
-},
+    var finalData = '';
+    //returning initial object
+    return {
+        ourFunction: function (createSong) {
+            finalData = "Welcome to the " + 
+                createSong.first + '. We\'ve got ' + 
+                createSong.second + ' and ' + 
+                createSong.third + '! We got ' + 
+                createSong.fourth + ' you need, ' + 
+                createSong.fifth + ' we know the ' +
+                createSong.sixth + '. ' + 'We are the ' + createSong.seventh + ' that can ' +
+                createSong.eighth + ' whatever you may ' +
+                createSong.ninth + '. If you got the ' +
+                createSong.tenth + ', ' + 
+                createSong.fifth + ', we got your ' +
+                createSong.eleventh + '. ' + 
+                createSong.first + ', welcome to the ' +
+                createSong.first + '. Watch it ' +
+                createSong.twelth + ' you to your....n-n-n-n...' +
+                createSong.thirteenth + ', ' + 
+                createSong.thirteenth + 's! ' + "We're gonna watch you " + createSong.fourteenth + '!'
+        },
 
-//returning the data
-returnWord: function(){
-  return finalData;
-}
+        //returning the data
+        returnWord: function () {
+            return finalData;
+        }
 
-//end return
-}
+        //end return
+    }
 
 });
